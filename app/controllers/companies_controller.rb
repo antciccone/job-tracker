74cc796ef29661b5f80479c749fe380a0a21efcf
antciccone <aciccone@bu.edu>
2016-10-20 15:55:21 -0600

    adding update and destroy

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -24,6 +24,7 @@ class CompaniesController < ApplicationController
 
   def edit
     @company = Company.find(params[:id])
+
   end
 
   def update
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,7 @@ class JobsController < ApplicationController
 
   def new
     @company = Company.find(params[:company_id])
-    @job = Job.new()
+    @job = Job.new
   end
 
   def create
@@ -21,19 +21,29 @@ class JobsController < ApplicationController
   end
 
   def show
+    @company = Company.find(params[:company_id])
     @job = Job.find(params[:id])
   end
 
   def edit
-    # implement on your own!
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job = Job.find(params[:id])
+    if @job.update_attributes(job_params)
+      redirect_to company_job_path
+    else
+      render :show
+    end
   end
 
   def destroy
-    # implement on your own!
+    @job = Job.find(params[:id])
+    @job.destroy
+
+    redirect_to company_jobs_path
   end
 
   private
