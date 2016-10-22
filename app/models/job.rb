bcf37fd6cba3aa7f226b1bc5021379d00bc6d430
antciccone <aciccone@bu.edu>
2016-10-21 19:12:57 -0600

    adding methods to companies and jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,12 @@ class Job < ActiveRecord::Base
   validates :title, :level_of_interest, presence: true
   belongs_to :company
   belongs_to :category
-  has_many :comments 
+  has_many :comments
+
+  def self.level_of_interest
+    group(:level_of_interest).count
+    # result = a.reduce([]) do |array, e|
+    #   array << e.first.to_s + "=>" + Job.find(e.second).title
+    #   array
+  end
 end
